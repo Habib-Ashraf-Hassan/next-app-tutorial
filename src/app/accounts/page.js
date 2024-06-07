6c4958eba92ca09f03b10d8b040714919ff73eed
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2024-06-07 10:53:44 +0300

    changes

---
+++ src/app/accounts/page.js
@@ -0,0 +1,11 @@
+
+export default function Accounts() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h1 className="styled-header">Accounts Page</h1>
+    <div>
+      <a href=".."><button className="next-page-button">Go back</button></a>
+    </div>
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
+'use client'
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
+
+function handleNavigate(){
+  route.push('..')
+}
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>Next.js Tutorial</h1>
+    <h1 className="styled-header">Next.js Tutorial</h1>
     <div>
-      <a href=""><button>Accounts Page</button></a>
+      <Link href={'accounts'}> <button className="next-page-button">Go to Accounts</button></Link>
+      <button className="next-page-button" onClick={handleNavigate}>Go back</button>
     </div>
     </main>
   );
