a801741fae401d708c7af8573b4a829221adecc8
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2024-06-07 21:01:52 +0300

    useRouter and Link done

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,19 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
-function handleNavigate(){
-  route.push('..')
-}
+
 
 export default function Home() {
+  const route = useRouter();
+  function handleNavigate(){
+    route.push('/accounts/admin')
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h1 className="styled-header">Next.js Tutorial</h1>
     <div>
       <Link href={'accounts'}> <button className="next-page-button">Go to Accounts</button></Link>
-      <button className="next-page-button" onClick={handleNavigate}>Go back</button>
+      <button className="next-page-button" onClick={handleNavigate}>Go to Admin</button>
     </div>
     </main>
   );
